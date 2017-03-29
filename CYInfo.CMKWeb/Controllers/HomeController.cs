936a2d7ee66c2191af41582b7b0dcae4493e0ea3
wanghanltd <wjz198602@hotmail.com>
2017-03-29 18:26:41 +0800

    3

--- CYInfo.CMKWeb/Controllers/HomeController.cs
+++ CYInfo.CMKWeb/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
 using System.Web.Mvc;
 
 namespace CYInfo.CMKWeb.Controllers
@@ -8,9 +10,24 @@ namespace CYInfo.CMKWeb.Controllers
     //[Authorize]
     public class HomeController : Controller
     {
+        static string apiUrl = ConfigurationManager.AppSettings["ApiUrl"].ToString();
+
         public ActionResult Index()
         {
-            return View();
+            string returnMessage = string.Empty;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(apiUrl);//测试环境
+                    returnMessage = client.GetAsync("/api/GetBrands").Result.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Content(returnMessage);
         }
     }
 }
---
+++ CYInfo.CMKWeb/Help_Codes/Common/DefaultMongoDb.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace CYInfo.CMKWeb.Help_Codes.Common
+{
+    public class DefaultMongoDb
+    {
+        public MongoDatabase database;
+        protected static MongoServer _server;
+        protected static MongoServerSettings _serverSettings;
+        public IMongoDatabase IDatabase;
+        public DefaultMongoDb(string databaseName)
+        {
+            try
+            {
+                string ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                MongoClient client = new MongoClient(ConnectionString);
+                IDatabase = client.GetDatabase(databaseName);
+
+                //string MongoIP = ConnectionString.Substring(10, ConnectionString.IndexOf(":", 10) - 10);
+                _serverSettings = new MongoServerSettings() { };
+                //_serverSettings.Server = new MongoServerAddress(MongoIP, 27017);
+                _serverSettings.Servers = IDatabase.Client.Settings.Servers;
+                _serverSettings.ReadPreference = IDatabase.Client.Settings.ReadPreference;
+
+
+                //最大连接池数量
+                _serverSettings.MaxConnectionPoolSize = 500;
+                //等待列队数量
+                _serverSettings.WaitQueueSize = 10;
+                MongoDatabaseSettings _databaseSettings = new MongoDatabaseSettings();
+                _server = new MongoServer(_serverSettings);
+                database = new MongoDatabase(_server, databaseName, _databaseSettings);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
