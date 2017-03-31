75a71e9fac488556669d95a87442902a155c0168
wanghanltd <wjz198602@hotmail.com>
2017-03-31 14:44:52 +0800

    uu

--- CYInfo.CMKWeb/Controllers/HomeController.cs
+++ CYInfo.CMKWeb/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CYInfo.CMKWeb.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -15,19 +18,52 @@ namespace CYInfo.CMKWeb.Controllers
         public ActionResult Index()
         {
             string returnMessage = string.Empty;
+             List<Brand> brandList = new List<Brand>();
+             Brand brand = new Brand();
             try
             {
                 using (var client = new HttpClient())
                 {
                     client.BaseAddress = new Uri(apiUrl);//测试环境
                     returnMessage = client.GetAsync("/api/GetBrands").Result.Content.ReadAsStringAsync().Result;
+
+
+                   JObject returnJsonEntities= JObject.Parse(returnMessage);
+
+                  
+
+                   List<Dictionary<string, object>> dataList = new List<Dictionary<string, object>>();
+                   dataList = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(returnJsonEntities["Message"].ToString());
+
+                   List<Dictionary<string, string>> brandNameList = new List<Dictionary<string, string>>();
+                  
+
+                  
+
+                    foreach(var entity in dataList)
+                    {
+                        Console.WriteLine(entity.ToString());
+                        brand = new Brand();
+                        brand.BrandPrefix = entity["BrandPrefix"].ToString();
+                        brandNameList = new List<Dictionary<string, string>>();
+                       
+                        brandNameList=JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(entity["Brands"].ToString());
+                        foreach (var brandEntity in brandNameList)
+                        {
+                            brand.BrandName.Add(brandEntity["BrandName"]);
+                        }
+
+                        brandList.Add(brand);
+                    }
+
+                   
                 }
             }
             catch (Exception ex)
             {
 
             }
-            return Content(returnMessage);
+            return View(brandList);
         }
     }
 }
