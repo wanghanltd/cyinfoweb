1f439d1cf4c7fc418f58a41fa4583e43569371fe
wanghanltd <wjz198602@hotmail.com>
2017-03-31 14:44:18 +0800

    fff

---
+++ CYInfo.CMKWeb/Models/Brand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CYInfo.CMKWeb.Models
+{
+    public class Brand
+    {
+
+        public Brand()
+        {
+            BrandName = new List<string>();
+        }
+        public string BrandPrefix { get; set; }
+
+        public List<string> BrandName { get; set; }
+
+
+    }
+
+    
+
+}
---
+++ CYInfo.CMKWeb/Models/BrandSizeChart.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CYInfo.CMKWeb.Models
+{
+    public class BrandSizeChart
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public virtual string Id { get; set; }
+
+
+        /// <summary>
+        /// 品牌名
+        /// </summary>
+        [Display(Name = "品牌名")]
+        public string BrandName { get; set; }
+
+        /// <summary>
+        /// 尺码表
+        /// </summary>
+        [Display(Name = "尺码表")]
+        public List<SizeChart> SizeCharts { get; set; }
+       
+
+
+
+    }
+
+
+    public class SizeChart
+    {
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+
+        [Display(Name = "Chart")]
+        public string Chart { get; set; }
+    }
+}
